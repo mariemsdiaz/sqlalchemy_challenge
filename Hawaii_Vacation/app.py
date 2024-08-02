629d2be4e2e428b7c99f49e470a4d2d4674054ea
Mariem Diaz <mariemdiaz@Mariems-Air.lan>
2024-08-01 22:14:45 -0700

    Module 10

--- Hawaii_Vacation/app.py
+++ Hawaii_Vacation/app.py
@@ -1,19 +1,28 @@
 # Import the dependencies.
 
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
@@ -22,9 +31,37 @@
 # Flask Setup
 #################################################
 
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Welcome to SQL Alchemty API, Created by Mariem Diaz<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]/[end_date format:yyyy-mm-dd]<br/>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+#Create the session link between python and DB    
+    session = Session(engine)
+
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= "2016-08-24").\
+        filter(Measurement.date <= "2017-08-23").\
+        order_by(Measurement.date).\
+        all()   
+
+    session.close()
+
+
